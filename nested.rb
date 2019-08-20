4c1864314a4eef0ede1b61a84ad39fae94469f22
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-20 13:44:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +84,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {}
 end
 
 def changing_alan
